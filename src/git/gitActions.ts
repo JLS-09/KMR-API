c71819fcad8fac0dc47bc0364b59d33819c42de4
JLS <121494017+JLS-09@users.noreply.github.com>
2025-06-22 01:20:48 +0200

    fix: indentation

--- src/git/gitActions.ts
+++ src/git/gitActions.ts
@@ -5,16 +5,16 @@ const git: SimpleGit = simpleGit().clean(CleanOptions.FORCE);
 
 export default async function performGitActions() {
   try {
-      if (!existsSync("./public/ckan-meta")) {
-        console.log('Cloning repository...');
-        await git.clone("https://github.com/KSP-CKAN/CKAN-meta.git", "./public/ckan-meta/");
-        console.log(`Repository cloned successfully`);
-      } else {
-        console.log("directory already exists, performing pull");
-        await git.pull("./public/ckan-meta");
-        console.log(`Pulling succesfull`);
-      }
-    } catch (error) {
-      console.error('Error cloning repository:', error.message);
+    if (!existsSync("./public/ckan-meta")) {
+      console.log('Cloning repository...');
+      await git.clone("https://github.com/KSP-CKAN/CKAN-meta.git", "./public/ckan-meta/");
+      console.log(`Repository cloned successfully`);
+    } else {
+      console.log("directory already exists, performing pull");
+      await git.pull("./public/ckan-meta");
+      console.log(`Pulling succesfull`);
     }
+  } catch (error) {
+      console.error('Error cloning repository:', error.message);
+  }
 }
